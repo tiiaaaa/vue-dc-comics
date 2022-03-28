10168726d705e0980e5b6437942d5783b9500b82
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-28 17:45:30 +0200

    stilizzazione Header component e modificato style app

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,124 @@
 <template>
-    <div>
-        header
+    <div class="header">
+        <div id="logo-wrapper">
+            <img src="../assets/img/dc-logo.png" alt="Logo Dc Comics">
+        </div>
+
+        <nav id="navbar-list">
+            <ul>
+                <li v-for="(link, index) in headerLinks" :key="index">
+                    <a href="#">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+
     </div>
 </template>
 
 <script>
 export default {
     name: 'IndexHeader',
+    data: function(){
+        return{
+            headerLinks:[
+                {
+                    text: "carachters",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "comics",
+                    url:"#",
+                    current: true
+                },
+
+                {
+                    text: "movies",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "tv",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "games",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "collectibles",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "videos",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "fans",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "news",
+                    url:"#",
+                    current: false
+                },
+
+                {
+                    text: "shop",
+                    url:"#",
+                    current: false
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+    div.header{
+        height: 15vh;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: aqua;
 
+        div#logo-wrapper{
+            height: 80px;
+            width: 80px;
+
+            img{
+                width: 100%;
+            }
+        }
+
+        nav#navbar-list{
+            ul{
+                display: flex;
+
+                li{
+                    list-style-type: none;
+                    margin-left: 1.2rem;
+
+                    a{
+                        font-size: 0.8rem;
+                        text-decoration: none;
+                        text-transform: uppercase;
+                        color: black;
+                    }
+                }
+            }
+        }
+    }
 </style>
