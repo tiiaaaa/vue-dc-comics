74fe2ac150f00004783931af7e2bc8acfbd32279
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-30 13:09:05 +0200

    stilizzato bottone sinistro del footer

--- src/components/FooterSocial.vue
+++ src/components/FooterSocial.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="footer-social">
-        
+        <section class="my-container">
+            
+            <a href="#" class="btn">
+                sign-up now!
+            </a>
+            
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -11,5 +25,24 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+@import '../assets/scss/partials/_variables.scss';
 
+    div#footer-social{
+        padding: 1.5rem 0;
+        background-color: $darkColor;
+
+        section.my-container{
+            display: flex;
+            justify-content: space-between;
+
+            a.btn{
+                padding: .8rem;
+                border: 3px solid $primaryColor;
+                color: white;
+                text-decoration: none;
+                text-transform: uppercase;
+                font-size: .8rem;
+            }
+        }
+    }
 </style>
